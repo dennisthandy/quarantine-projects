a33336e69dd0693d29443499bebf6a245f0b3085
Dennis Thandy Nur Cahyono <dennisthandy@yahoo.co.id>
2020-07-05 06:54:47 +0700

    fixing image

--- assets/data.js
+++ assets/data.js
@@ -2,183 +2,183 @@ export const data = [
   {
     title: 'Template Project',
     link: './projects/a-template',
-    image: 'a-template.jpg'
+    image: 'a-template.JPG'
   },
   {
     title: 'Alert Design',
     link: './projects/alert-design',
-    image: 'alert-design.jpg'
+    image: 'alert-design.JPG'
   },
   {
     title: 'Background Color Changes',
     link: './projects/background-changes',
-    image: 'background-changes.jpg'
+    image: 'background-changes.JPG'
   },
   {
     title: 'Biner to Decimal',
     link: './projects/biner-to-decimal',
-    image: 'biner-decimal.jpg'
+    image: 'biner-decimal.JPG'
   },
   {
     title: 'Border Radius Setting',
     link: './projects/border-radius',
-    image: 'border-radius.jpg'
+    image: 'border-radius.JPG'
   },
   {
     title: 'Calculator',
     link: './projects/calculator',
-    image: 'calculator.jpg'
+    image: 'calculator.JPG'
   },
   {
     title: 'Christmas Light',
     link: './projects/christmas-light',
-    image: 'christmas-light.jpg'
+    image: 'christmas-light.JPG'
   },
   {
     title: 'Circle Spinner',
     link: './projects/circle-spinner',
-    image: 'circle-spinner.jpg'
+    image: 'circle-spinner.JPG'
   },
 
   {
     title: 'Covid ID Stats',
     link: './projects/covid-id',
-    image: 'covid-id.jpg'
+    image: 'covid-id.JPG'
   },
   {
     title: 'Random Tebakan Receh',
     link: './projects/crispy-jokes',
-    image: 'crispy-jokes.jpg'
+    image: 'crispy-jokes.JPG'
   },
   {
     title: 'Drink Water',
     link: './projects/drink-water',
-    image: 'drink-water.jpg'
+    image: 'drink-water.JPG'
   },
   {
     title: 'Facebook Card',
     link: './projects/facebook-card',
-    image: 'facebook-card.jpg'
+    image: 'facebook-card.JPG'
   },
   {
     title: 'Falling Snowflake',
     link: './projects/falling-snowflake',
-    image: 'falling-snowflake.jpg'
+    image: 'falling-snowflake.JPG'
   },
   {
     title: 'Fireworks Launcher',
     link: './projects/fireworks-launcher',
-    image: 'fireworks-launcher.jpg'
+    image: 'fireworks-launcher.JPG'
   },
   {
     title: 'Gifs Cat',
     link: './projects/gif-cat',
-    image: 'gif-cat.jpg'
+    image: 'gif-cat.JPG'
   },
   {
     title: 'Hexa Color Generator',
     link: './projects/hexa-color-generator',
-    image: 'hexa-color-generator.jpg'
+    image: 'hexa-color-generator.JPG'
   },
   {
     title: 'Hover Board',
     link: './projects/hover-board',
-    image: 'hover-board.jpg'
+    image: 'hover-board.JPG'
   },
   {
     title: 'Hover Button',
     link: './projects/hover-button',
-    image: 'hover-button.jpg'
+    image: 'hover-button.JPG'
   },
   {
     title: 'Hamburger Menu',
     link: './projects/hamburger-menu',
-    image: 'hamburger-menu.jpg'
+    image: 'hamburger-menu.JPG'
   },
   {
     title: 'Instagram Card',
     link: './projects/instagram-card',
-    image: 'instagram-card.jpg'
+    image: 'instagram-card.JPG'
   },
   {
     title: 'Jump Loading',
     link: './projects/jump-loading',
-    image: 'jump-loading.jpg'
+    image: 'jump-loading.JPG'
   },
   {
     title: 'Love Button',
     link: './projects/love-button',
-    image: 'love-button.jpg'
+    image: 'love-button.JPG'
   },
   {
     title: 'Meal Generator',
     link: './projects/meal-generator',
-    image: 'meal-generator.jpg'
+    image: 'meal-generator.JPG'
   },
   {
     title: 'Night Day Slider',
     link: './projects/night-day',
-    image: 'night-day.jpg'
+    image: 'night-day.JPG'
   },
   {
     title: 'Raindrop',
     link: './projects/raindrop',
-    image: 'raindrop.jpg'
+    image: 'raindrop.JPG'
   },
   {
     title: 'Rating Design',
     link: './projects/rating-design',
-    image: 'rating-design.jpg'
+    image: 'rating-design.JPG'
   },
   {
     title: 'RGB Color Generator',
     link: './projects/rgb-color-generator',
-    image: 'rgb-color-generator.jpg'
+    image: 'rgb-color-generator.JPG'
   },
 
   {
     title: 'Scale Smile Sider',
     link: './projects/scale-smile',
-    image: 'scale-smile.jpg'
+    image: 'scale-smile.JPG'
   },
   {
     title: 'Text Animation',
     link: './projects/text-animation',
-    image: 'text-animation.jpg'
+    image: 'text-animation.JPG'
   },
   {
     title: 'Daily TODO List',
     link: './projects/todo-list',
-    image: 'todo-list.jpg'
+    image: 'todo-list.JPG'
   },
   {
     title: 'Toogle Theme',
     link: './projects/toggle-theme',
-    image: 'toggle-theme.jpg'
+    image: 'toggle-theme.JPG'
   },
   {
     title: 'Tooltip',
     link: './projects/tooltip',
-    image: 'tooltip.jpg'
+    image: 'tooltip.JPG'
   },
   {
     title: 'Trinity Web',
     link: './projects/trinity-web',
-    image: 'trinity-web.jpg'
+    image: 'trinity-web.JPG'
   },
   {
     title: 'Tap Virus',
     link: './projects/virus-game',
-    image: 'virus-game.jpg'
+    image: 'virus-game.JPG'
   },
   {
     title: 'Wanna See Rainbow',
     link: './projects/wanna-see-rainbow',
-    image: 'wanna-see-rainbow.jpg'
+    image: 'wanna-see-rainbow.JPG'
   },
   {
     title: 'Wash Hand Steps',
     link: './projects/wash-hand-step',
-    image: 'wash-hand-step.jpg'
+    image: 'wash-hand-step.JPG'
   },
 ]
